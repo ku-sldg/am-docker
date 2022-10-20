eb42f7b88a75ac7933594d579459bc91d7e08617
garrettmills <shout@garrettmills.dev>
2022-10-20 00:36:18 -0500

    Improve automatic replacement of blockchain port number

--- scripts/setup-io-stub.sh
+++ scripts/setup-io-stub.sh
@@ -9,3 +9,7 @@ sed -i "s/val\suserAddress\s=\s\".*\"/val userAddress = \"0x$ACCT_ID\"/g" "${AT_
 sed -i "s/val\shealthRecordContract\s=\s\".*\"/val healthRecordContract = \"$HEALTH_ID\"/g" "${AT_PREFIX}/am-cakeml/stubs/IO_Stubs_extra.sml"
 sed -i "s/val\sblockchainIpPort\s=\s.*/val blockchainIpPort = ${BLK_HTTP_PORT}/g" "${AT_PREFIX}/am-cakeml/stubs/IO_Stubs_extra.sml"
 
+sed -i "s/8543/${BLK_HTTP_PORT}/g" "${AT_PREFIX}/am-cakeml/apps/blockchain/config.ini"
+sed -i "s/8543/${BLK_HTTP_PORT}/g" "${AT_PREFIX}/am-cakeml/apps/blockchain/Blockchain.sml"
+sed -i "s/8543/${BLK_HTTP_PORT}/g" "${AT_PREFIX}/am-cakeml/util/Http.sml"
+
