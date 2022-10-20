3c08de1588fc2a74ccd52b34cad338672db46be5
garrettmills <shout@garrettmills.dev>
2022-10-20 00:48:17 -0500

    Allow customizing demo server port using env var at setup

--- scripts/setup-io-stub.sh
+++ scripts/setup-io-stub.sh
@@ -4,6 +4,7 @@ AT_PREFIX="${AT_PREFIX:-}"
 ACCT_ID="$(cat ${AT_PREFIX}/ku-mst/pnet/geth-public-address.out)"
 HEALTH_ID="$(cat ${AT_PREFIX}/ku-mst/pnet/health-records.address)"
 BLK_HTTP_PORT="${BLK_HTTP_PORT:-8543}"
+DEMO_SERVER_PORT="${DEMO_SERVER_PORT:-5000}"
 
 sed -i "s/val\suserAddress\s=\s\".*\"/val userAddress = \"0x$ACCT_ID\"/g" "${AT_PREFIX}/am-cakeml/stubs/IO_Stubs_extra.sml"
 sed -i "s/val\shealthRecordContract\s=\s\".*\"/val healthRecordContract = \"$HEALTH_ID\"/g" "${AT_PREFIX}/am-cakeml/stubs/IO_Stubs_extra.sml"
@@ -12,4 +13,7 @@ sed -i "s/val\sblockchainIpPort\s=\s.*/val blockchainIpPort = ${BLK_HTTP_PORT}/g
 sed -i "s/8543/${BLK_HTTP_PORT}/g" "${AT_PREFIX}/am-cakeml/apps/blockchain/config.ini"
 sed -i "s/8543/${BLK_HTTP_PORT}/g" "${AT_PREFIX}/am-cakeml/apps/blockchain/Blockchain.sml"
 sed -i "s/8543/${BLK_HTTP_PORT}/g" "${AT_PREFIX}/am-cakeml/util/Http.sml"
+
+sed -i "s/port\s=\s5000/port = ${DEMO_SERVER_PORT}/g" "${AT_PREFIX}/am-cakeml/apps/demo/server/example_server.ini"
+sed -i "s/=>\s5000/=> ${DEMO_SERVER_PORT}/g" "${AT_PREFIX}/am-cakeml/am/CoplandCommUtil.sml"
 
